4869cb9629db5728cd33f18a5462f1bdb668dc8b
Tianyun Zhang (Code) <doowzs@qq.com>
2020-04-15 08:15:11 +0000

    weighted random action for AI

--- src/clients/ai.cpp
+++ src/clients/ai.cpp
@@ -3,16 +3,30 @@
 
 #include <client.h>
 #include <clients/ai.h>
+#include <common.h>
 #include <object.h>
 #include <objects/base.h>
 #include <objects/tank.h>
 #include <player.h>
 #include <server.h>
 
+#include <chrono>
+#include <random>
+
 AIClient::AIClient(const Server *server, const char *name, double difficulty)
     : Client(name, 0), server(server) {
   thinking_frames = thinking_countdown =
       (int)(difficulty * server->fps) < 0 ? 1 : (int)(difficulty * server->fps);
+  rng.seed(std::chrono::system_clock::now().time_since_epoch().count());
+
+  random_actions.emplace_back(std::make_pair(20, ACTION_SHOOT));
+  random_actions.emplace_back(std::make_pair(10, ACTION_MOVE_UP));
+  random_actions.emplace_back(std::make_pair(10, ACTION_MOVE_DOWN));
+  random_actions.emplace_back(std::make_pair(10, ACTION_MOVE_LEFT));
+  random_actions.emplace_back(std::make_pair(10, ACTION_MOVE_RIGHT));
+  for (auto it = random_actions.begin() + 1; it != random_actions.end(); ++it) {
+    it->first = (it - 1)->first + it->first;  // prefix sum
+  }
 }
 
 enum PlayerAction AIClient::act() {
@@ -24,33 +38,48 @@ enum PlayerAction AIClient::act() {
   } else {
     thinking_countdown = thinking_frames;
   }
-  {
-    // search for target
-    for (auto &player : server->players) {
-      if (player->getClient() == this) {
-        tank = player->getTank();
-      } else {
-        target = player->getBase();
+  if (std::uniform_real_distribution<double>(0.0, 1.0)(rng) < 0.3) {
+    return actRandomly();
+  } else {
+    {
+      // search for target
+      for (auto &player : server->players) {
+        if (player->getClient() == this) {
+          tank = player->getTank();
+        } else {
+          target = player->getBase();
+        }
+      }
+      if (tank == nullptr or target == nullptr) {
+        // no tank or target in game, AI go to sleep
+        return ACTION_IDLE;
       }
     }
-    if (tank == nullptr or target == nullptr) {
-      // no tank or target in game, AI go to sleep
-      return ACTION_IDLE;
-    }
-  }
-  {
-    if (tank->pos_y < target->pos_y - 1) {
-      return ACTION_MOVE_DOWN;
-    } else if (tank->pos_y > target->pos_y + target->height + 1) {
-      return ACTION_MOVE_UP;
-    } else {
-      if (tank->pos_x < target->pos_x) {
-        return tank->direction == D_RIGHT ? ACTION_SHOOT : ACTION_MOVE_RIGHT;
+    {
+      if (tank->pos_y < target->pos_y - 1) {
+        return ACTION_MOVE_DOWN;
+      } else if (tank->pos_y > target->pos_y + target->height + 1) {
+        return ACTION_MOVE_UP;
       } else {
-        return tank->direction == D_LEFT ? ACTION_SHOOT : ACTION_MOVE_LEFT;
+        if (tank->pos_x < target->pos_x) {
+          return tank->direction == D_RIGHT ? ACTION_SHOOT : ACTION_MOVE_RIGHT;
+        } else {
+          return tank->direction == D_LEFT ? ACTION_SHOOT : ACTION_MOVE_LEFT;
+        }
       }
     }
   }
+}
+
+enum PlayerAction AIClient::actRandomly() {
+  int lottery =
+      std::uniform_int_distribution<int>(1, random_actions.back().first)(rng);
+  for (auto action : random_actions) {
+    if (lottery <= action.first) {
+      return action.second;
+    }
+  }
+  Panic("should not reach here");
 }
 
 bool AIClient::post(__attribute__((unused)) int now) { return true; }
--- src/headers/clients/ai.h
+++ src/headers/clients/ai.h
@@ -6,6 +6,9 @@
 
 #include <client.h>
 
+#include <random>
+#include <vector>
+
 // forward declaration
 class Server;
 
@@ -13,10 +16,13 @@ class AIClient : public Client {
  private:
   const Server *server;                     // read-only access to server
   int thinking_frames, thinking_countdown;  // frames until next action
+  std::default_random_engine rng;           // random number generator
+  std::vector<std::pair<int, enum PlayerAction>> random_actions;
 
  public:
   AIClient(const Server *server, const char *name, double difficulty);
   enum PlayerAction act();
+  enum PlayerAction actRandomly();
   bool post(int now);
   bool post(int now, const Object *object);
   bool post(int now, const Player *player);
