22d629382db8f2bab380484169ff5fb7322c8461
Tianyun Zhang (Code) <doowzs@qq.com>
2020-04-15 07:18:12 +0000

    update life of base to 100

--- src/objects/base.cpp
+++ src/objects/base.cpp
@@ -11,15 +11,17 @@ Base::Base(Server *server, Player *player, int pos_y, int pos_x)
     : Object(server, player, OBJECT_BASE, pos_y, pos_x, 5, 5,
              "+---+"
              "|X X|"
-             "| 9 |"
+             "| ? |"
              "|X X|"
              "+---+",
-             9) {}
+             100) {}
 
 void Base::operator()() {}
 
 void Base::operator()(__attribute__((unused)) Object *object) {}
 
 void Base::update() {
-  pattern[12] = (char)('0' + life);
+  char life_str[8] = "";
+  sprintf(life_str, "%03d", life);
+  strncpy(pattern + 11, life_str, 3);
 }
